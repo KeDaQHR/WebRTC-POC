90eefae579b46dd54d7ca598b5e35955ec7cc43e
KE MA <ke.ma@qhrtech.com>
2019-02-28 16:43:31 -0500

    clean up videoController

--- Sources/Screens/VideoScreen/VideoViewController.swift
+++ Sources/Screens/VideoScreen/VideoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class VideoViewController: UIViewController {
 	
-	@IBOutlet private weak var localVideoView: UIView?
+	@IBOutlet private var localVideoView: UIView!
 	@IBOutlet private var statusLabel: UILabel!
 	@IBOutlet private var videoControlButton: UIButton!
 	@IBOutlet private var endCallButton: UIButton!
@@ -45,38 +45,33 @@ class VideoViewController: UIViewController {
 		
 		#if arch(arm64)
 		// Using metal (arm64 only)
-		let localRenderer = RTCMTLVideoView(frame: self.localVideoView?.frame ?? CGRect.zero)
-		let remoteRenderer = RTCMTLVideoView(frame: self.view.frame)
+		let localRenderer = RTCMTLVideoView(frame: localVideoView?.frame ?? CGRect.zero)
+		let remoteRenderer = RTCMTLVideoView(frame: view.frame)
 		localRenderer.videoContentMode = .scaleAspectFill
 		remoteRenderer.videoContentMode = .scaleAspectFill
 		#else
 		// Using OpenGLES for the rest
-		let localRenderer = RTCEAGLVideoView(frame: self.localVideoView?.frame ?? CGRect.zero)
-		let remoteRenderer = RTCEAGLVideoView(frame: self.view.frame)
+		let localRenderer = RTCEAGLVideoView(frame: localVideoView?.frame ?? CGRect.zero)
+		let remoteRenderer = RTCEAGLVideoView(frame: view.frame)
 		#endif
 		
-		self.webRTCClient.startCaptureLocalVideo(renderer: localRenderer)
-		self.webRTCClient.renderRemoteVideo(to: remoteRenderer)
+		webRTCClient.startCaptureLocalVideo(renderer: localRenderer)
+		webRTCClient.renderRemoteVideo(to: remoteRenderer)
 		
-		if let localVideoView = self.localVideoView {
-			self.embedView(localRenderer, into: localVideoView)
-		}
-		self.embedView(remoteRenderer, into: self.view)
-		self.view.sendSubviewToBack(remoteRenderer)
+		embedView(localRenderer, into: localVideoView)
+		embedView(remoteRenderer, into: view)
+		view.sendSubviewToBack(remoteRenderer)
 	}
 	
 	private func embedView(_ view: UIView, into containerView: UIView) {
 		containerView.addSubview(view)
 		view.translatesAutoresizingMaskIntoConstraints = false
-		containerView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[view]|",
-																																options: [],
-																																metrics: nil,
-																																views: ["view":view]))
-		
-		containerView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view]|",
-																																options: [],
-																																metrics: nil,
-																																views: ["view":view]))
+		NSLayoutConstraint.activate([
+			view.topAnchor.constraint(equalTo: containerView.topAnchor),
+			view.leftAnchor.constraint(equalTo: containerView.leftAnchor),
+			view.rightAnchor.constraint(equalTo: containerView.rightAnchor),
+			view.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+		])
 		containerView.layoutIfNeeded()
 	}
 }
