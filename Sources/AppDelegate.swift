1c719930fd6e06a1b6a2e7395f01dfdfef1e0bcd
KE MA <ke.ma@qhrtech.com>
2019-03-04 10:42:45 -0500

    Add gitIgnore

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -11,8 +11,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	
+	// We use Google's public stun servers. For production apps you should deploy your own stun/turn servers.
+	fileprivate let defaultIceServers = ["stun:stun.l.google.com:19302",
+																			 "stun:stun1.l.google.com:19302",
+																			 "stun:stun2.l.google.com:19302",
+																			 "stun:stun3.l.google.com:19302",
+																			 "stun:stun4.l.google.com:19302"]
+	
 	private var window: UIWindow?
-	private let config = Config.default
 	
 	func application(_ application: UIApplication,
 									 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -21,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		let IPInputPage = IPInputController()
 		IPInputPage.onContinuePerformed = { [weak self] ipAddress in
 			guard let self = self else { return }
-			let mainPage = self.buildMainViewController(with: ipAddress)
+			guard let mainPage = self.buildMainViewController(with: ipAddress) else { return }
 			window.rootViewController?.present(mainPage, animated: true)
 		}
 		
@@ -31,11 +37,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		return true
 	}
 	
-	private func buildMainViewController(with IPAddress: String) -> UIViewController {
-		let signalClient = SignalingClient(serverUrl: URL(string: "ws://\(IPAddress)/")!)
-		let webRTCClient = WebRTCClient(iceServers: config.webRTCIceServers)
+	private func buildMainViewController(with IPAddress: String) -> UIViewController? {
+		// Set this to the machine's address which runs the signaling server
+		guard let defaultSignalingServerUrl = URL(string: "ws://\(IPAddress)/") else {
+			return nil
+		}
+		
+		let signalClient = SignalingClient(serverUrl: defaultSignalingServerUrl)
+		let webRTCClient = WebRTCClient(iceServers: defaultIceServers)
 		let mainViewController = MainViewController(signalClient: signalClient,
 																								webRTCClient: webRTCClient)
+		
 		let navViewController = UINavigationController(rootViewController: mainViewController)
 		navViewController.navigationBar.isTranslucent = false
 		if #available(iOS 11.0, *) {
